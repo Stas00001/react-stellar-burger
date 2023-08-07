b7640fc9f75aa018357e54286aba2a977f887889
Stas00001 <116278430+Stas00001@users.noreply.github.com>
2023-08-07 16:47:11 +0300

    Updates

--- src/services/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { ingredientsReducer } from './ingredients';
-import { ingredientsDetailsReducer } from './ingeridents-details';
-import { orderReducer } from './order';
-export const rootReducer = combineReducers({
-    ingredients: ingredientsReducer,
-    ingredientsDetails: ingredientsDetailsReducer,
-    order: orderReducer,
-  });
